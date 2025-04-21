112edadae8ceb4ef75a5f4746971ccf0f09e7f74
MADHU BALAN S <stmmadhubalan@gmail.com>
2025-04-21 22:23:55 +0530

    Setup Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfV9MjVNPCcGbfqJHyZ2EGGMSZVVw4IiU",
+  authDomain: "form-tracker-3bbd6.firebaseapp.com",
+  projectId: "form-tracker-3bbd6",
+  storageBucket: "form-tracker-3bbd6.firebasestorage.app",
+  messagingSenderId: "446738816800",
+  appId: "1:446738816800:web:984e94ae95921af0d24145",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
